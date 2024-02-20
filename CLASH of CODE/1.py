5787feacd6c7733026e4705cc027751c82835610
YUNDAEWON1 <dwsb0905@naver.com>
2024-02-20 13:19:21 +0900

    clash of code

--- CLASH of CODE/1.py
+++ CLASH of CODE/1.py
@@ -4,12 +4,17 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-t = int(input())
-for i in range(t):
-    n, k = [int(j) for j in input().split()]
-    print(k - n % k)
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+x = int(input())
+x += 1
+while not is_prime(x):
+    x += 1
 
-# print("The number of students for each testcase.")
+print(x)
